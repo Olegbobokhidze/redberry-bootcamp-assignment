fb76bba6dc378c73389f92dacf6b2a967caaa3a2
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-19 21:53:42 +0400

    style: blog card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,12 @@
-
-
 <script setup>
 import HomeHero from '../components/home/HomeHero.vue'
 import HomeBlogFilter from '../components/home/HomeBlogFilter.vue'
+import BlogCard from '../components/blog/BlogCard.vue'
 </script>
 
 <template>
-    <div className="px-[76px] w-full">
-        <HomeHero />
-        <HomeBlogFilter />
-    </div>
+  <div className="px-[76px] w-full">
+    <HomeHero />
+    <HomeBlogFilter />
+  </div>
 </template>
