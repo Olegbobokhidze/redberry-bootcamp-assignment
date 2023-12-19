5dc900b8539a7019ee0a77f7d0defd5564f8f747
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-19 15:16:46 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div className="w-[10rem] bg-slate-500 h-screen self-center">
+    <div className="text-black">test</div>
+  </div>
+</template>
+
+<style scoped></style>
