6decedd5b745f30ad4132ccc7d02194d30c6e217
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-19 16:16:06 +0400

    feat: add folder structure and icons

---
+++ src/components/icons/IconChecked.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg width="26" height="18" viewBox="0 0 26 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1.66602 9.00001L9.21268 16.5467L24.3327 1.45334"
+      stroke="white"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
