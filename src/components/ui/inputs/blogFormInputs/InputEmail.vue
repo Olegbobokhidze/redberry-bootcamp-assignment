4fd35f8cf742a4b686f5fe2766d972e68df8d3f9
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-21 23:36:32 +0400

    feat: add inputs

--- src/components/ui/inputs/blogFormInputs/InputEmail.vue
+++ src/components/ui/inputs/blogFormInputs/InputEmail.vue
@@ -1,6 +1,50 @@
 <template>
-<div></div>
+  <div class="flex flex-col gap-2 self-start">
+    <label class="font-bold text-[#1A1A1F]">{{ title }}</label>
+    <input
+      v-model="value"
+      @input="onInput"
+      :type="type || 'text'"
+      :placeholder="placeholder"
+      :class="[
+        'rounded-xl border w-[288px] h-[44px] px-4 py-2 text-[#1A1A1F]',
+        'focus:outline-none focus:border-[#5D37F3] focus:bg-[#F7F7FF]',
+        { 'border-gray-300': !isInvalid, 'border-red-500 bg-[#FAF2F3]': isInvalid && wasTouched }
+      ]"
+      @blur="wasTouched = true"
+    />
+    <p :class="endsWithClass">{{ endsWithMessage }}</p>
+  </div>
 </template>
-<script>
 
+<script setup>
+import { ref, computed } from 'vue'
+import { useField } from 'vee-validate'
+
+const props = defineProps({
+  name: String,
+  type: String,
+  title: String,
+  placeholder: String
+})
+
+const { value, errorMessage } = useField(props.name)
+const wasTouched = ref(false)
+
+const onInput = (event) => {
+  value.value = event.target.value
+}
+
+const isInvalid = computed(() => errorMessage.value !== undefined)
+
+const minLengthRule = computed(
+  () => (value.value || '').length >= 4 || value.value === '' || value.value === undefined
+)
+// გასაკეთებელია
+const endsWithClass = computed(() => ({
+  'text-gray-500': minLengthRule.value,
+  'text-red-500': !minLengthRule.value && wasTouched
+}))
+
+const endsWithMessage = computed(() => 'უნდა მთავრდებოდეს @redberry.ge')
 </script>
--- src/views/BlogFormView.vue
+++ src/views/BlogFormView.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="w-full flex items-center justify-center">
-
+  <div class="w-full flex justify-center relative px-[76px] py-[40px]">
+    <div
+      class="w-[44px] h-[44px] cursor-pointer left-[76px] absolute rounded-[50%] bg-[#E4E3EB] hover:bg-[#D9D8E0] flex items-center justify-center "
+    >
+      <IconArrowLeft />
+    </div>
     <FormWrapper />
   </div>
 </template>
 
 <script setup>
 import FormWrapper from '@/components/blogForm/FormWrapper.vue'
+import IconArrowLeft from '@/components/icons/IconArrowLeft.vue'
 </script>
