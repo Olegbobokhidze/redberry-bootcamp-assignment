bad16feed79773518a8bbc1349dddcabd3d5b635
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-23 14:22:14 +0400

    feat: add shared navigator and blogcard style

--- src/views/BlogFormView.vue
+++ src/views/BlogFormView.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="w-full flex justify-center relative px-[76px] py-[33px]">
-    <div
-      class="w-[44px] h-[44px] cursor-pointer left-[76px] absolute rounded-[50%] bg-[#E4E3EB] hover:bg-[#D9D8E0] flex items-center justify-center "
-    >
-      <IconArrowLeft />
-    </div>
+    <RouteNavigator route="/" />
     <FormWrapper />
   </div>
 </template>
 
 <script setup>
 import FormWrapper from '@/components/blogForm/FormWrapper.vue'
-import IconArrowLeft from '@/components/icons/IconArrowLeft.vue'
+import RouteNavigator from '@/components/shared/RouteNavigator.vue'
 </script>
