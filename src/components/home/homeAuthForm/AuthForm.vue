50b09fd7e0ce428c7717b7e8fb4a3c105361e3a1
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-24 21:14:16 +0400

    feat: add user in localstorage

--- src/components/home/homeAuthForm/AuthForm.vue
+++ src/components/home/homeAuthForm/AuthForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="mx-[24px] my-[40px] ">
+  <form class="mx-[24px] my-[40px]">
     <div class="flex flex-col gap-6 w-full items-center justify-center">
       <p class="text-[#1A1A1F] text-[24px] font-bold">შესვლა</p>
       <InputAuthEmail
@@ -39,12 +39,17 @@ export default {
     const modalStore = useModalStore()
     const authStore = useAuthStore()
     const [email, emailProps] = defineField('authEmail')
+    authStore.initializeFromLocalStorage()
+
     const onSubmit = handleSubmit(async (values) => {
       try {
         const result = await authenticateWithEmail(values.authEmail)
         if (result.success) {
           console.log('Authentication successful')
           authStore.login({ email: values.authEmail })
+
+          // Save authentication information to localStorage
+          authStore.saveToLocalStorage()
         } else {
           console.error('Authentication failed:', result.error)
         }
