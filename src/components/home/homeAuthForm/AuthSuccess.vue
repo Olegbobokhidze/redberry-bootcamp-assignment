ba2e120d42320ff39089f39f7ee620a8b6c54638
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-24 20:31:40 +0400

    feat: add auth success modal and route for auth user

---
+++ src/components/home/homeAuthForm/AuthSuccess.vue
@@ -0,0 +1,30 @@
+import { useModalStore } from '@/stores/ModalStore';
+<template>
+  <div class="flex items-center flex-col justify-center mt-[64px] mx-[24px] mb-[40px]">
+    <div class="bg-[#14D81C] w-[53.33px] h-[53.33px] rounded-[50%] flex items-center justify-center mb-[16px]">
+      <IconChecked />
+    </div>
+    <p class="text-[#1A1A1F] font-bold text-[20px] mb-[48px]">წარმატებული ავტორიზაცია</p>
+    <button
+      class="w-full h-[40px] bg-[#5D37F3] hover:bg-[#512BE7] font-bold text-white text-[14px] rounded-lg py-[10px] px-[20px]"
+      @click="modalStore.closeModal"
+    >
+      კარგი
+    </button>
+  </div>
+</template>
+<script>
+import IconChecked from '@/components/icons/IconChecked.vue'
+import { useModalStore } from '@/stores/ModalStore'
+export default {
+  setup() {
+    const modalStore = useModalStore()
+    return {
+      modalStore
+    }
+  },
+  components: {
+    IconChecked
+  }
+}
+</script>
--- src/components/shared/PageHeader.vue
+++ src/components/shared/PageHeader.vue
@@ -1,10 +1,32 @@
-<script setup>
+<script>
+import { computed } from 'vue'
 import ButtonPageHeader from '../ui/buttons/ButtonPageHeader.vue'
+import { useRouter } from 'vue-router'
+export default {
+  setup() {
+    const router = useRouter()
+    const onFormPage = computed(() => router.currentRoute.value.name === 'PostBlog')
+    return {
+      onFormPage
+    }
+  },
+  components: {
+    ButtonPageHeader
+  }
+}
 </script>
 
 <template>
-  <div class="w-screen h-[80px] bg-white items-center flex px-[76px] py-[28px] justify-between">
+  <div
+    :class="{
+      'w-screen h-[80px] bg-white items-center flex px-[76px] py-[28px] justify-between':
+        !onFormPage,
+      'w-screen h-[80px] bg-white items-center flex px-[76px] py-[28px] justify-center': onFormPage
+    }"
+  >
     <img alt="logo" src="../../assets/images/redberrylogo.png" class="w-[150px] h-[24px]" />
-    <ButtonPageHeader />
+    <div v-if="!onFormPage">
+      <ButtonPageHeader />
+    </div>
   </div>
 </template>
