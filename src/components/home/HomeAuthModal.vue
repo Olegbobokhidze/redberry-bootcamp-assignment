8a8f8d063f1604d3db251d25cd3e235fd1b32991
olegbobokhidze <oleg.bobokhidze.1@iliauni.edu.ge>
2023-12-24 16:17:13 +0400

    feat: add modal

---
+++ src/components/home/HomeAuthModal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="store.isModalOpen">
+    <div
+      class="fixed w-full h-full flex items-center justify-center bg-black bg-opacity-20  top-0 left-0 text-black"
+    >
+      <div class="bg-red-500 w-[500px] h-[400px] z-50 text-black">
+        <p>This is the modal content!</p>
+        <button @click="store.closeModal">Close Modal</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useModalStore } from '@/stores/ModalStore'
+export default {
+  setup() {
+    const store = useModalStore()
+    return {
+      store
+    }
+  }
+}
+</script>
--- src/components/ui/buttons/ButtonPageHeader.vue
+++ src/components/ui/buttons/ButtonPageHeader.vue
@@ -1,4 +1,20 @@
-<script setup></script>
 <template>
-  <button class="bg-[#5D37F3] rounded-[8px] text-white w-[93px] h-[40px] hover:bg-[#512BE7]">შესვლა</button>
+  <button
+    @click="store.openModal"
+    class="bg-[#5D37F3] rounded-[8px] text-white w-[93px] h-[40px] hover:bg-[#512BE7]"
+  >
+    შესვლა
+  </button>
 </template>
+<script>
+import { useModalStore } from '@/stores/ModalStore'
+
+export default {
+  setup() {
+    const store = useModalStore()
+    return {
+      store
+    }
+  }
+}
+</script>
